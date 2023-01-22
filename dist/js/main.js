f40612b0375883cac7c58dba5f32220fd5478317
Felix Hellström <felixhellstrom@macbook-air.lan>
2023-01-22 13:17:38 +0100

    update 1.1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,12 @@
 console.log("main.js init");
 
 const callFunction = async () => {
-  const url = "/.netlify/functions/function";
   try {
-    const response = await fetch(url);
+    const response = await fetch("/.netlify/functions/function");
     const data = await response.text();
     console.log(data);
+    const h1 = document.querySelector("h1");
+    h1.textContent = data;
   } catch (err) {
     console.error(err);
   }
