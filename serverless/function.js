10dac8dc88f03039d6b1cfe5bb9aecbba0443806
Felix Hellström <felixhellstrom@MacBook-Air.lan>
2023-01-24 10:07:17 +0100

    message

--- serverless/function.js
+++ serverless/function.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   try {
     console.log(event.body);
-    console.log("init");
+    console.log("init-number-2");
     return {
       statusCode: 200,
     };
